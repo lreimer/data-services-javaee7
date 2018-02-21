5b9ca5d5572cd86a8ca1e57c11e432fe1ab6e8fe
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2018-02-21 22:22:14 +0100

    Removed redundant code.

--- hazelcast/src/main/java/de/qaware/qacampus/jvm/RestAPI.java
+++ hazelcast/src/main/java/de/qaware/qacampus/jvm/RestAPI.java
@@ -1,25 +1,11 @@
 package de.qaware.qacampus.jvm;
 
-import org.glassfish.jersey.jackson.JacksonFeature;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * The REST API application.
  */
 @ApplicationPath("/api/")
 public class RestAPI extends Application {
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> classes = new HashSet<>();
-
-        classes.add(JacksonFeature.class);
-        classes.add(HelloResource.class);
-        classes.add(HeapResource.class);
-
-        return classes;
-    }
 }
